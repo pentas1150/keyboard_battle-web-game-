c3d243bbdb1c8c1c084f804d2e1490bbdf7d9bc7
pentas1150 <beewol@soongsil.ac.kr>
2020-09-02 15:07:45 +0900

    1)main.ejs에 domain, port 변수 추가

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
+require("dotenv").config();
 
 /* GET home page. */
 router.get("/", (req, res, next) => {
-    res.render("main", { memberLists: req.app.get("memberList") });
+    res.render("main", { memberLists: req.app.get("memberList"), domain: process.env.DOMAIN, port: req.app.get("port") });
 });
 
 module.exports = router;
